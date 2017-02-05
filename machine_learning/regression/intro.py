859ec1019b7f8c9e91986b27b4bcbf8be8cf8b2e
prudywsh <=>
2017-02-05 23:12:58 +0100

    predict next prices + use matplotlib to show data on a graph

--- machine_learning/regression/intro.py
+++ machine_learning/regression/intro.py
@@ -1,9 +1,14 @@
 import pandas as pd
 import quandl
 import math
+import datetime
 import numpy as np
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
 
 # df = DataFrame (pandas)
 df = quandl.get('WIKI/GOOGL')
@@ -23,16 +28,21 @@ df.fillna(-99999, inplace=True)
 forecast_out = int(math.ceil(0.01*len(df)))
 
 df['label'] = df[forecast_col].shift(-forecast_out)
-# drop rows where there is no labels
-df.dropna(inplace=True)
 
 # features : everything except label column
 X = np.array(df.drop(['label'], 1))
-# labels : the label column of course
-y = np.array(df['label'])
 
 # normalization
 X = preprocessing.scale(X)
+
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
+
+# drop rows where there is no labels
+df.dropna(inplace=True)
+
+# labels : the label column of course
+y = np.array(df['label'])
 
 # fit classifier with 20% of our data
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
@@ -42,4 +52,27 @@ clf.fit(X_train, y_train)
 # test classifier (accuracy)
 accuracy = clf.score(X_test, y_test)
 
-print(accuracy)
+print('accuracy : ' + str(accuracy))
+
+# predict
+forecast_set = clf.predict(X_lately)
+print(forecast_set, accuracy, forecast_out)
+
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.savefig('file.svg') # or plt.show()
