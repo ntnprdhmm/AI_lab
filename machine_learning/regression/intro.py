4ec14b9008279c05d1b199788da9e8f837cab350
prudywsh <=>
2017-02-05 16:36:29 +0100

    ML regresion : label created. The goal is to predict Adj close in the future

--- machine_learning/regression/intro.py
+++ machine_learning/regression/intro.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import quandl
+import math
 
+# df = DataFrame (pandas)
 df = quandl.get('WIKI/GOOGL')
 
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
@@ -10,4 +12,13 @@ df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.
 # just get the column that we care about
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 
-print(df.head())
+forecast_col = 'Adj. Close'
+# replace NA/NaN with a value (cause we can't work with NA/Nan in machine learning)
+df.fillna(-99999, inplace=True)
+
+# try to predict 10% of the dataframe
+forecast_out = int(math.ceil(0.01*len(df)))
+
+df['label'] = df[forecast_col].shift(-forecast_out)
+df.dropna(inplace=True)
+print(df.tail())
