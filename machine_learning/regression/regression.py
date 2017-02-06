2a77d5261bced1ed21fff037f6eb44a51bb70f56
prudywsh <=>
2017-02-06 22:03:55 +0100

    ML regression : calculate the accuracy of the best fit line

--- machine_learning/regression/regression.py
+++ machine_learning/regression/regression.py
@@ -15,9 +15,17 @@ def slope(xs, ys):
 def y_intersect(m, xs, ys):
     return np.mean(ys) - m * np.mean(xs)
 
-
 def best_fit_line(x, m, b):
     return (m*x) + b
+
+def squared_error(ys_origin, ys_predicted):
+    return sum((ys_predicted - ys_origin)**2)
+
+def r_squared(ys_origin, ys_predicted):
+    ys_origin_mean = [np.mean(ys_origin) for y in ys_origin]
+    squared_error_regr = squared_error(ys_origin, ys_predicted)
+    squared_error_y_mean = squared_error(ys_origin, ys_origin_mean)
+    return 1 - (squared_error_regr / squared_error_y_mean)
 
 m = slope(xs, ys)
 b = y_intersect(m, xs, ys)
@@ -26,6 +34,9 @@ regression_line = [best_fit_line(x, m, b) for x in xs]
 
 predict_x = 8
 predict_y = best_fit_line(predict_x, m, b)
+
+r_squared = r_squared(ys, regression_line)
+print(r_squared)
 
 plt.scatter(predict_x, predict_y, color='g')
 plt.scatter(xs, ys)
